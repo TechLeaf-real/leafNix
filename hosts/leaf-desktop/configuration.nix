61d125327df77a21e1b462d9d2930f75a4b72a02
Techleaf <mikiediggins@gmail.com>
2025-07-17 09:05:12 +0100

    "316 2025-07-17 09:04:59 25.11.20250714.62e0f05 6.15.6"

--- hosts/leaf-desktop/configuration.nix
+++ hosts/leaf-desktop/configuration.nix
@@ -73,8 +73,8 @@
       current=$(nixos-rebuild list-generations --json | jq '.[] | select (.current == true) | "\(.generation) \(.date) \(.nixosVersion) \(.kernelVersion)"')
       git commit -am "$current"
       git push origin master
-      clear
       popd
+      clear
     '')
     (writeShellScriptBin "update" ''
       #! nix-shell -i bash -p bash
@@ -83,23 +83,9 @@
       clear
       echo "Updating Flake..."
       nix flake update
-      echo "NixOS Rebuilding..."
-      set +o pipefail
-      sudo nixos-rebuild switch 2>&1 | tee .nixos-switch.log
-      set -o pipefail
-      echo  -e "\n\033[34mNixOS rebuild completed\033[0m"
-      echo -ne "\rExit in 3" && sleep 1
-      echo -ne "\rExit in 2" && sleep 1
-      echo -ne "\rExit in 1" && sleep 1
-      echo -ne "\033[?1049l"
-      clear
-      rm .nixos-switch.log
-      clear
-      current=$(nixos-rebuild list-generations --json | jq '.[] | select (.current == true) | "\(.generation) \(.date) \(.nixosVersion) \(.kernelVersion)"')
-      git commit -am "$current"
-      git push origin master
-      clear
+      rebuild
       popd
+      clear
     '')
   ])
 
