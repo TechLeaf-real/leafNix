6e88c406ff32c863282f447ffdac6cd3d04d92e8
Techleaf <mikiediggins@gmail.com>
2025-07-18 02:53:22 +0100

    "329 2025-07-18 02:53:14 25.11.20250714.62e0f05 6.15.6"

--- modules/home-manager/fish.nix
+++ modules/home-manager/fish.nix
@@ -31,7 +31,7 @@
         programs.bat = {
             enable = true;
             config = {
-
+                theme = "mocha";
             };
             themes = {
                 catppuccin-mocha = {
