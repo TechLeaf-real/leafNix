4e0314a2fcb1194399a145a42c17af0aadd15817
Techleaf <mikiediggins@gmail.com>
2025-07-19 08:27:11 +0100

    "370 2025-07-19 04:53:25 25.11.20250716.6e98748 6.15.6"

---
+++ modules/nixos/stylix.nix
@@ -0,0 +1,32 @@
+{ lib, config, pkgs, ...}:
+
+{
+  stylix = {
+    enable = true;
+    base16Scheme = "${pkgs.base16-schemes}/share/themes/catppuccin-mocha.yaml";
+    image = ~/Pictures/Wallpapers/catppuccin-wallpapers-main/os/nix-black-4k.png;
+    polarity = "dark";
+    fonts = {
+      serif = {
+        package = pkgs.dejavu_fonts;
+        name = "Dejavu Serif";
+      };
+
+      sansSerif = {
+        package = pkgs.dejavu_fonts;
+        name = "Dejavu Sans";
+      };
+
+      monospace = {
+        package = pkgs.dejavu_fonts;
+        name = "Dejavu Sans Mono";
+      };
+
+      emoji = {
+        package = pkgs.noto-fonts-emoji;
+        name = "Noto Color Emoji";
+      };
+
+    };
+  };
+}
