a11e6c6fecb9f15027b4542eec94c0cb56e41d18
Techleaf <mikiediggins@gmail.com>
2025-07-20 06:56:01 +0100

    "403 2025-07-20 06:55:55 25.11.20250716.6e98748 6.15.6"

--- modules/home-manager/fastfetch.nix
+++ modules/home-manager/fastfetch.nix
@@ -8,7 +8,45 @@
   home.file = {
     ".config/fastfetch/config.jsonc" = {
         text = ''
-
+          {
+            "logo": {
+              "type": "auto",
+            },
+            "display": {
+              "brightColor": true,
+            },
+            "modules": [
+              "title",
+              "separator",
+              "os",
+              "host",
+              "kernel",
+              "uptime",
+              "packages",
+              "shell",
+              "display",
+              "de",
+              "wm",
+              "wmtheme",
+              "theme",
+              "icons",
+              "font",
+              "cursor",
+              "terminal",
+              "terminalfont",
+              "cpu",
+              "gpu",
+              "memory",
+              "swap",
+              "disk",
+              "localip",
+              "battery",
+              "poweradapter",
+              "locale",
+              "break",
+              "colors"
+            ]
+          }
         '';
       enable = true;
     };
