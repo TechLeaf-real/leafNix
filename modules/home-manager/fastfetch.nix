f728a045c0cab80f53658c83072cd99a4eecac8e
Techleaf <mikiediggins@gmail.com>
2025-07-20 06:30:00 +0100

    "395 2025-07-20 06:26:32 25.11.20250716.6e98748 6.15.6"

--- modules/home-manager/fastfetch.nix
+++ modules/home-manager/fastfetch.nix
@@ -6,45 +6,48 @@
   ];
 
   home.file = {
-    "~/.config/fastfetch/config.jsonc".text = ''
-      "$schema": "https://github.com/fastfetch-cli/fastfetch/raw/dev/doc/json_schema.json", // Optional: Provides IDE intelligence
-      "logo": {
-        "type": "auto",
-      },
-      "display": {
-        "brightColor": true,
-      },
-      "modules": [
-        "title",
-        "separator",
-        "os",
-        "host",
-        "kernel",
-        "uptime",
-        "packages",
-        "shell",
-        "display",
-        "de",
-        "wm",
-        "wmtheme",
-        "theme",
-        "icons",
-        "font",
-        "cursor",
-        "terminal",
-        "terminalfont",
-        "cpu",
-        "gpu",
-        "memory",
-        "swap",
-        "disk",
-        "localip",
-        "battery",
-        "poweradapter",
-        "locale",
-        "break",
-        "colors"
-      ]
-    '';
+    "~/.config/fastfetch/config.jsonc" = {
+        text = ''
+          "$schema": "https://github.com/fastfetch-cli/fastfetch/raw/dev/doc/json_schema.json", // Optional: Provides IDE intelligence
+          "logo": {
+            "type": "auto",
+          },
+          "display": {
+            "brightColor": true,
+          },
+          "modules": [
+            "title",
+            "separator",
+            "os",
+            "host",
+            "kernel",
+            "uptime",
+            "packages",
+            "shell",
+            "display",
+            "de",
+            "wm",
+            "wmtheme",
+            "theme",
+            "icons",
+            "font",
+            "cursor",
+            "terminal",
+            "terminalfont",
+            "cpu",
+            "gpu",
+            "memory",
+            "swap",
+            "disk",
+            "localip",
+            "battery",
+            "poweradapter",
+            "locale",
+            "break",
+            "colors"
+          ]
+        '';
+      enable = true;
+    };
   };
 }
