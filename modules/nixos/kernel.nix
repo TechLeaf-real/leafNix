b5bb0dee6d983d34579fba4992f12c4ace0fa08b
Techleaf <mikiediggins@gmail.com>
2025-07-21 05:14:12 +0100

    "431 2025-07-21 05:09:44 25.11.20250719.c87b95e 6.15.6-zen1"

--- modules/nixos/kernel.nix
+++ modules/nixos/kernel.nix
@@ -3,7 +3,7 @@
   ...
 }: {
   boot = {
-    kernelPackages = pkgs.linuxPackages_latest;
+    kernelPackages = pkgs.linuxPackages_zen;
     # kernelParams = [
     #   "quiet"
     #   "vt.global_cursor_default=0"
