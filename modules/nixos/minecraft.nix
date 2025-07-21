ecb5e22069c7b62bfd6fc1fdc2a2fc0ebdbd6dbf
Techleaf <mikiediggins@gmail.com>
2025-07-21 16:52:15 +0100

    "454 2025-07-21 16:16:54 25.11.20250719.c87b95e 6.15.6-zen1"

--- modules/nixos/minecraft.nix
+++ modules/nixos/minecraft.nix
@@ -2,7 +2,7 @@
   services.minecraft-server = {
     enable = true;
     # package = pkgs.papermc;
-    openFirewall = true;
+    # openFirewall = true;
     whitelist = {
       TechLeaf = "49448e8d-f881-4233-b587-be44990288b3";
     };
