6572694fc17f14a5f2c9d554bc3d2e18e8a12072
Techleaf <mikiediggins@gmail.com>
2025-07-21 16:09:39 +0100

    "453 2025-07-21 16:08:14 25.11.20250719.c87b95e 6.15.6-zen1"

--- modules/home-manager/fastfetch.nix
+++ modules/home-manager/fastfetch.nix
@@ -4,45 +4,42 @@
   ];
 
   home.file = {
-    ".config/fastfetch/config.jsonc" = {
-      text = ''
-        {
-          "logo": {
-            "type": "auto",
-          },
-          "display": {
-            "brightColor": true,
-          },
-          "modules": [
-            "title",
-            "separator",
-            "os",
-            "host",
-            "kernel",
-            "uptime",
-            "packages",
-            "shell",
-            "de",
-            "wm",
-            "wmtheme",
-            "icons",
-            "cursor",
-            "terminal",
-            "terminalfont",
-            "cpu",
-            "gpu",
-            "memory",
-            "swap",
-            "disk",
-            "localip",
-            "battery",
-            "poweradapter",
-            "break",
-            "colors"
-          ]
-        }
-      '';
-      enable = true;
-    };
+    ".config/fastfetch/config.jsonc".text = ''
+      {
+        "logo": {
+          "type": "auto",
+        },
+        "display": {
+          "brightColor": true,
+        },
+        "modules": [
+          "title",
+          "separator",
+          "os",
+          "host",
+          "kernel",
+          "uptime",
+          "packages",
+          "shell",
+          "de",
+          "wm",
+          "wmtheme",
+          "icons",
+          "cursor",
+          "terminal",
+          "terminalfont",
+          "cpu",
+          "gpu",
+          "memory",
+          "swap",
+          "disk",
+          "localip",
+          "battery",
+          "poweradapter",
+          "break",
+          "colors"
+        ]
+      }
+    '';
   };
 }
