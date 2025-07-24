df38f0701a75e373b9c56644205b39a31d018d30
Techleaf <mikiediggins@gmail.com>
2025-07-25 00:21:46 +0100

    "480 2025-07-24 19:27:43 25.11.20250723.fc02ee7 6.15.7"

--- modules/nixos/emulation.nix
+++ modules/nixos/emulation.nix
@@ -1,15 +1,23 @@
-{pkgs, ...}: {
+{
+  pkgs,
+  pkgs-stable,
+  ...
+}: {
   options = {
   };
 
   config = {
-    environment.systemPackages = with pkgs; [
-      retroarch-full
-      ryubing
-      cemu
-      dolphin-emu
-      rpcs3
-      xemu
-    ];
+    environment.systemPackages =
+      (with pkgs; [
+        retroarch-full
+        ryubing
+        cemu
+        dolphin-emu
+        rpcs3
+        xemu
+      ])
+      ++ (with pkgs-stable; [
+        # emulationstation-de
+      ]);
   };
 }
