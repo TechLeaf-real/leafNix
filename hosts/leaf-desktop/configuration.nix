4c3b716d8a76fc76c2a01d66a157126deac9b451
Techleaf <mikiediggins@gmail.com>
2025-07-31 02:03:33 +0100

    "536 2025-07-31 02:03:25 25.11.20250728.dc96378 6.15.8"

--- hosts/leaf-desktop/configuration.nix
+++ hosts/leaf-desktop/configuration.nix
@@ -66,55 +66,59 @@
       magic-wormhole-rs
       libreoffice-qt6
       mpv
-      (writeShellScriptBin "rebuild" ''
-        #! nix-shell -i bash -p bash
-        set -e
-        pushd ~/leafNix
-        clear
-        sudo deadnix -e
-        clear
-        if git diff --quiet '*.nix'; then
-            echo "\nNo changes detected, exiting."
-            popd
-            exit 0
-        fi
-        clear
-        git diff -U0 '*.nix'
-        set +o pipefail
-        nh os switch ~/leafNix || exit 0
-        set -o pipefail
-        echo  -e "\n\033[34mNixOS rebuild completed\033[0m"
-        echo -ne "\rExit in 1" && sleep 1
-        echo -ne "\033[?1049l"
-        clear
-        alejandra *
-        clear
-        current=$(nixos-rebuild list-generations --json | jq '.[] | select (.current == true) | "\(.generation) \(.date) \(.nixosVersion) \(.kernelVersion)"')
-        git commit -am "$current"
-        git push origin master
-        popd
-        clear
-      '')
-      (writeShellScriptBin "update" ''
-        #! nix-shell -i bash -p bash
-        set -e
-        pushd ~/leafNix
-        clear
-        echo "Updating Flake..."
-        nix flake update
-        set +o pipefail
-        nh os switch ~/leafNix || exit 0
-        set -o pipefail
-        echo  -e "\n\033[34mNixOS rebuild completed\033[0m"
-        echo -ne "\rExit in 1" && sleep 1
-        echo -ne "\033[?1049l"
-        clear
-        current=$(nixos-rebuild list-generations --json | jq '.[] | select (.current == true) | "\(.generation) \(.date) \(.nixosVersion) \(.kernelVersion)"')
-        git commit -am "$current"
-        git push origin master
-        popd
-        clear
-      '')
+      # (writeShellScriptBin "rebuild" ''
+      #   #! nix-shell -i bash -p bash
+      #   set -e
+      #   pushd ~/leafNix
+      #   clear
+      #   sudo deadnix -e
+      #   clear
+      #   if git diff --quiet '*.nix'; then
+      #       echo "\nNo changes detected, exiting."
+      #       popd
+      #       exit 0
+      #   fi
+      #   clear
+      #   git diff -U0 '*.nix'
+      #   set +o pipefail
+      #   nh os switch ~/leafNix || exit 0
+      #   set -o pipefail
+      #   echo  -e "\n\033[34mNixOS rebuild completed\033[0m"
+      #   echo -ne "\rExit in 1" && sleep 1
+      #   echo -ne "\033[?1049l"
+      #   clear
+      #   alejandra *
+      #   clear
+      #   current=$(nixos-rebuild list-generations --json | jq '.[] | select (.current == true) | "\(.generation) \(.date) \(.nixosVersion) \(.kernelVersion)"')
+      #   git commit -am "$current"
+      #   git push origin master
+      #   popd
+      #   clear
+      # '')
+      # (writeShellScriptBin "update" ''
+      #   #! nix-shell -i bash -p bash
+      #   set -e
+      #   pushd ~/leafNix
+      #   clear
+      #   echo "Updating Flake..."
+      #   nix flake update
+      #   set +o pipefail
+      #   nh os switch ~/leafNix || exit 0
+      #   set -o pipefail
+      #   echo  -e "\n\033[34mNixOS rebuild completed\033[0m"
+      #   echo -ne "\rExit in 1" && sleep 1
+      #   echo -ne "\033[?1049l"
+      #   clear
+      #   current=$(nixos-rebuild list-generations --json | jq '.[] | select (.current == true) | "\(.generation) \(.date) \(.nixosVersion) \(.kernelVersion)"')
+      #   git commit -am "$current"
+      #   git push origin master
+      #   popd
+      #   clear
+      # '')
+    ])
+    ++ (with inputs.self.packages.x86_64-linux; [
+      rebuild
+      update
     ])
     ++ (with pkgs-stable; [
       unityhub
