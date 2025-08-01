467e4f268470c3eec2bdc2e29b91e597d2cd326f
Techleaf <mikiediggins@gmail.com>
2025-08-01 03:01:05 +0100

    "551 2025-08-01 03:00:57 25.11.20250728.dc96378 6.15.8"

--- modules/nixos/ai.nix
+++ modules/nixos/ai.nix
@@ -2,7 +2,7 @@
   services.ollama = {
     enable = true;
     acceleration = "rocm";
-    home = "/home/techleaf";
+    # home = "/home/techleaf";
     openFirewall = true;
     loadModels = [
       "deepseek-r1:14b"
