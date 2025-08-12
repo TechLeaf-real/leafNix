d827fdc5ebfd61012ff3e4321998f42eab08c1aa
Techleaf <mikiediggins@gmail.com>
2025-08-12 17:42:11 +0100

    "596 2025-08-12 11:31:35 25.11.20250803.5b09dc4 6.16.0"

--- modules/nixos/server/traefik.nix
+++ modules/nixos/server/traefik.nix
@@ -1,31 +1,5 @@
 {...}: {
   services.traefik = {
     enable = true;
-    staticConfigOptions = {
-      entryPoints = {
-        web = {
-          address = ":80";
-          asDefault = true;
-          http.redirections.entrypoint = {
-            to = "websecure";
-            scheme = "https";
-          };
-        };
-
-        websecure = {
-          address = ":443";
-          asDefault = true;
-          # https.tls.certResolver = "";
-        };
-      };
-
-      api.dashboard = true;
-      api.insecure = true;
-    };
-
-    dynamicConfigOptions = {
-      # http.routers = {};
-      # http.services = {};
-    };
   };
 }
