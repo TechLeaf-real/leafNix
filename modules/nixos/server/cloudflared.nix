b7be127e21a571e937741b41dd046e56520baba1
Techleaf <mikiediggins@gmail.com>
2025-08-13 17:41:22 +0100

    "596 2025-08-12 11:31:35 25.11.20250803.5b09dc4 6.16.0"

--- modules/nixos/server/cloudflared.nix
+++ modules/nixos/server/cloudflared.nix
@@ -5,8 +5,7 @@
       "4296602e-eee8-4837-9196-6f40dd895506" = {
         credentialsFile = "/root/.cloudflared/4296602e-eee8-4837-9196-6f40dd895506.json";
         ingress = {
-          "media.techleaf.dev" = "http://192.168.1.239:8096";
-          "seerr.techleaf.dev" = "http://192.168.1.239:5055";
+          "media.techleaf.dev" = "http://localhost:8096";
         };
         default = "http_status:404";
       };
