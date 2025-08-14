6575c93c00d35e598cb287b085073bfab5052e29
Techleaf <mikiediggins@gmail.com>
2025-08-14 18:30:59 +0100

    "599 2025-08-14 00:45:20 25.11.20250803.5b09dc4 6.16.0"

--- modules/nixos/server/nginx.nix
+++ modules/nixos/server/nginx.nix
@@ -10,6 +10,8 @@
         proxyPass = "http://localhost:8096";
         proxyWebsockets = true;
       };
+      sslCertificate = "/root/techleaf.dev.pem";
+      sslCertificateKey = "/root/techleaf.dev.key";
     };
   };
   networking.firewall.allowedTCPPorts = [80 443];
