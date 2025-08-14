55aa57ce1d61e64fd45ffb80a686415a6a64cc03
Techleaf <mikiediggins@gmail.com>
2025-08-14 18:27:45 +0100

    "599 2025-08-14 00:45:20 25.11.20250803.5b09dc4 6.16.0"

--- modules/nixos/server/nginx.nix
+++ modules/nixos/server/nginx.nix
@@ -1,29 +1,14 @@
 {...}: {
-  security.acme = {
-    acceptTerms = true;
-    # certs."techleaf.dev" = {
-    #   email = "mikiediggins@gmail.com";
-    #   dnsProvider = "cloudflare";
-    #   dnsResolver = "1.1.1.1:53";
-    #   environmentFile = "/var/lib/acme/cloudflare.s";
-    #   webroot = null;
-    # };
-  };
-
   services.nginx = {
     enable = true;
     recommendedProxySettings = true;
-    recommendedTlsSettings = true;
-    virtualHosts."techleaf.dev" = {
-      enableACME = true;
+    recommendedTLSSettings = true;
+
+    virtualHosts."media.techleaf.dev" = {
       forceSSL = true;
-      locations = {
-        "/api/" = {
-          proxyPass = "http://127.0.0.1:8080";
-        };
-        "/media/" = {
-          proxyPass = "http://127.0.0.1:8096";
-        };
+      locations."/" = {
+        proxyPass = "http://localhost:8096";
+        proxyWebsockets = true;
       };
     };
   };
