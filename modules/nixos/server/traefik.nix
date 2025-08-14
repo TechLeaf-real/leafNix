f0a78ee9bed8f74a8e9d3994a494b890e0622880
Techleaf <mikiediggins@gmail.com>
2025-08-14 16:51:11 +0100

    "599 2025-08-14 00:45:20 25.11.20250803.5b09dc4 6.16.0"

--- modules/nixos/server/traefik.nix
+++ modules/nixos/server/traefik.nix
@@ -49,25 +49,25 @@
           };
         };
       };
-      # tls = {
-      #   options = {
-      #     default = {
-      #       cipherSuites = [
-      #         "TLS_CHACHA20_POLY1305_SHA256"
-      #         "TLS_AES_256_GCM_SHA384"
-      #         "TLS_AES_128_GCM_SHA256"
-      #       ];
-      #     };
-      #   };
-      #   stores = {
-      #     default = {
-      #       defaultCertificate = {
-      #         certFile = "/root/techleaf.dev.pem";
-      #         keyFile = "/root/techleaf.dev.key";
-      #       };
-      #     };
-      #   };
-      # };
+      tls = {
+        options = {
+          default = {
+            cipherSuites = [
+              "TLS_CHACHA20_POLY1305_SHA256"
+              "TLS_AES_256_GCM_SHA384"
+              "TLS_AES_128_GCM_SHA256"
+            ];
+          };
+        };
+        stores = {
+          default = {
+            defaultCertificate = {
+              certFile = "/root/techleaf.dev.pem";
+              keyFile = "/root/techleaf.dev.key";
+            };
+          };
+        };
+      };
     };
   };
 
