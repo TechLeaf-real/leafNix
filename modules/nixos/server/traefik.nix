96f5de6973bd45e1971e6a5a63e3bbb2ca3d889c
Techleaf <mikiediggins@gmail.com>
2025-08-14 16:40:14 +0100

    "599 2025-08-14 00:45:20 25.11.20250803.5b09dc4 6.16.0"

--- modules/nixos/server/traefik.nix
+++ modules/nixos/server/traefik.nix
@@ -53,7 +53,9 @@
         options = {
           default = {
             cipherSuites = [
-              "TLS_AEAD-CHACHA20-POLY1305-SHA256"
+              "TLS_CHACHA20_POLY1305_SHA256"
+              "TLS_AES_256_GCM_SHA384"
+              "TLS_AES_128_GCM_SHA256"
             ];
           };
         };
