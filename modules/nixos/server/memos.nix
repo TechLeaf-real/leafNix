0476d6a0ba11ccdc95b7aac68077002212fd0564
Techleaf <mikiediggins@gmail.com>
2025-08-24 23:03:50 +0100

    "637 2025-08-24 22:47:25 25.11.20250819.2007595 6.16.1"

--- modules/nixos/server/memos.nix
+++ modules/nixos/server/memos.nix
@@ -2,6 +2,7 @@
   systemd.services.memos = {
     enable = true;
     after = ["network.target"];
+    wantedBy = ["default.target"];
     serviceConfig = {
       Type = "simple";
       ExecStart = ''${pkgs.memos}/bin/memos'';
