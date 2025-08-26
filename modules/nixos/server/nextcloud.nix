65d4d4cebfc02bd13ca33946b9414899d7b530a8
Techleaf <mikiediggins@gmail.com>
2025-08-26 02:38:27 +0100

    "664 2025-08-26 02:29:36 25.11.20250819.2007595 6.16.1"

--- modules/nixos/server/nextcloud.nix
+++ modules/nixos/server/nextcloud.nix
@@ -2,5 +2,11 @@
   services.nextcloud = {
     enable = true;
     home = "/pool/dataset/nextcloud";
+    config = {
+      adminuser = "techleaf";
+    };
+    settings = {
+      trusted_proxies = ["192.168.1.239"];
+    };
   };
 }
