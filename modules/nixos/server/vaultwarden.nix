51cc553e5119e800d2c03ce8c9c5f989be1bd45c
Techleaf <mikiediggins@gmail.com>
2025-08-29 03:07:02 +0100

    "670 2025-08-27 15:33:12 25.11.20250825.3b9f00d 6.16.3"

--- modules/nixos/server/vaultwarden.nix
+++ modules/nixos/server/vaultwarden.nix
@@ -2,11 +2,11 @@
   services.vaultwarden = {
     enable = true;
     config = {
-      DOMAIN = "http://localhost:8071";
+      DOMAIN = "https://pass.techleaf.dev";
       # SIGNUPS_ALLOWED = false;
       ROCKET_ADDRESS = "127.0.0.1";
       ROCKET_PORT = 8222;
     };
   };
-  networking.firewall.allowedTCPPorts = [8071];
+  networking.firewall.allowedTCPPorts = [8222];
 }
