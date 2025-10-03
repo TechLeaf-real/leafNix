8a7dacdd5d12834b0f7e30d8ee54a799da3276b1
Techleaf <mikiediggins@gmail.com>
2025-10-03 23:54:13 +0100

    "723 2025-10-03 23:54:08 25.11.20251002.7df7ff7 6.16.9"

--- hosts/leaf-desktop/configuration.nix
+++ hosts/leaf-desktop/configuration.nix
@@ -139,15 +139,15 @@
     ];
   };
 
-  xdg.mime.enable = true;
-  xdg.mime.defaultApplications = {
-    "text/html" = "firefox.desktop";
-    "x-scheme-handler/http" = "firefox.desktop";
-    "x-scheme-handler/https" = "firefox.desktop";
-    "x-scheme-handler/about" = "firefox.desktop";
-    "x-scheme-handler/unknown" = "firefox.desktop";
-  };
-  environment.sessionVariables.DEFAULT_BROWSER = "${pkgs.firefox}/bin/firefox";
+  # xdg.mime.enable = true;
+  # xdg.mime.defaultApplications = {
+  #   "text/html" = "firefox.desktop";
+  #   "x-scheme-handler/http" = "firefox.desktop";
+  #   "x-scheme-handler/https" = "firefox.desktop";
+  #   "x-scheme-handler/about" = "firefox.desktop";
+  #   "x-scheme-handler/unknown" = "firefox.desktop";
+  # };
+  # environment.sessionVariables.DEFAULT_BROWSER = "${pkgs.firefox}/bin/firefox";
 
   programs.kdeconnect = {
     enable = true;
