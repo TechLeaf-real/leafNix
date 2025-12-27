ca336599af4373946e67ab49568c47c2721211cb
Techleaf <mikiediggins@gmail.com>
2025-12-27 13:08:30 +0000

    "809 2025-12-26 20:22:48 26.05.20251225.3e2499d 6.18.2"

--- modules/nixos/server/media.nix
+++ modules/nixos/server/media.nix
@@ -3,79 +3,79 @@
   pkgs-stable,
   ...
 }: {
-  # services.jellyfin = {
-  #   enable = true;
-  #   openFirewall = false;
-  # };
-
-  services.declarative-jellyfin = {
+  services.jellyfin = {
     enable = true;
-    openFirewall = true;
-    network = {
-      knownProxies = ["192.168.1.239"];
-    };
-    system = {
-      serverName = "LeafNet Streaming";
-      pluginRepositories = [
-        {
-          content = {
-            Enabled = true;
-            Name = "Jellyfin Stable";
-            Url = "https://repo.jellyfin.org/files/plugin/manifest.json";
-          };
-          tag = "RepositoryInfo";
-        }
-        {
-          content = {
-            Enabled = true;
-            Name = "Intro Skipper";
-            Url = "https://intro-skipper.org/manifest.json";
-          };
-          tag = "RepositoryInfo";
-        }
-      ];
-    };
-    users = {
-      techleaf = {
-        mutable = false;
-        permissions.isAdministrator = true;
-        hashedPassword = "$PBKDF2-SHA512$iterations=210000$4BCE7FB5229C58DF3FA49CA4660803E2$10CD54D9DAE819548CAB50D0F1F09E0F4B964C3BDB73D9DDD3667CBE7194BA02C2D25C89520C8FB7C31E4AF42CAA8882D8DD75839922EB4EE12713DC29D01616";
-      };
-    };
-    encoding = {
-      enableHardwareEncoding = true;
-      hardwareAccelerationType = "vaapi";
-      enableDecodingColorDepth10Hevc = true;
-      allowHevcEncoding = true;
-      hardwareDecodingCodecs = [
-        "h264"
-        "hevc"
-        "mpeg2video"
-        "vc1"
-        "vp9"
-        "av1"
-      ];
-      enableVppTonemapping = true;
-      enableTonemapping = true;
-    };
-    libraries = {
-      Movies = {
-        enabled = true;
-        contentType = "movies";
-        pathInfos = ["/pool/dataset/media/movies"];
-      };
-      Shows = {
-        enabled = true;
-        contentType = "tvshows";
-        pathInfos = ["/pool/dataset/media/shows"];
-      };
-      Music = {
-        enabled = true;
-        contentType = "music";
-        pathInfos = ["/pool/dataset/media/music" "/pool/dataset/media/newMusic" "/pool/dataset/media/rippedMusic"];
-      };
-    };
+    openFirewall = false;
   };
+
+  # services.declarative-jellyfin = {
+  #   enable = true;
+  #   openFirewall = true;
+  #   network = {
+  #     knownProxies = ["192.168.1.239"];
+  #   };
+  #   system = {
+  #     serverName = "LeafNet Streaming";
+  #     pluginRepositories = [
+  #       {
+  #         content = {
+  #           Enabled = true;
+  #           Name = "Jellyfin Stable";
+  #           Url = "https://repo.jellyfin.org/files/plugin/manifest.json";
+  #         };
+  #         tag = "RepositoryInfo";
+  #       }
+  #       {
+  #         content = {
+  #           Enabled = true;
+  #           Name = "Intro Skipper";
+  #           Url = "https://intro-skipper.org/manifest.json";
+  #         };
+  #         tag = "RepositoryInfo";
+  #       }
+  #     ];
+  #   };
+  #   users = {
+  #     techleaf = {
+  #       mutable = false;
+  #       permissions.isAdministrator = true;
+  #       hashedPassword = "$PBKDF2-SHA512$iterations=210000$4BCE7FB5229C58DF3FA49CA4660803E2$10CD54D9DAE819548CAB50D0F1F09E0F4B964C3BDB73D9DDD3667CBE7194BA02C2D25C89520C8FB7C31E4AF42CAA8882D8DD75839922EB4EE12713DC29D01616";
+  #     };
+  #   };
+  #   encoding = {
+  #     enableHardwareEncoding = true;
+  #     hardwareAccelerationType = "vaapi";
+  #     enableDecodingColorDepth10Hevc = true;
+  #     allowHevcEncoding = true;
+  #     hardwareDecodingCodecs = [
+  #       "h264"
+  #       "hevc"
+  #       "mpeg2video"
+  #       "vc1"
+  #       "vp9"
+  #       "av1"
+  #     ];
+  #     enableVppTonemapping = true;
+  #     enableTonemapping = true;
+  #   };
+  #   libraries = {
+  #     Movies = {
+  #       enabled = true;
+  #       contentType = "movies";
+  #       pathInfos = ["/pool/dataset/media/movies"];
+  #     };
+  #     Shows = {
+  #       enabled = true;
+  #       contentType = "tvshows";
+  #       pathInfos = ["/pool/dataset/media/shows"];
+  #     };
+  #     Music = {
+  #       enabled = true;
+  #       contentType = "music";
+  #       pathInfos = ["/pool/dataset/media/music" "/pool/dataset/media/newMusic" "/pool/dataset/media/rippedMusic"];
+  #     };
+  #   };
+  # };
 
   services.radarr = {
     enable = true;
