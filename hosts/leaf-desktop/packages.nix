f71dfdb5544afbdfbece99844555dfc5082c88a1
Techleaf <mikiediggins@gmail.com>
2025-12-30 23:56:58 +0000

    "810 2025-12-30 23:56:53 26.05.20251225.3e2499d 6.18.2"

--- hosts/leaf-desktop/packages.nix
+++ hosts/leaf-desktop/packages.nix
@@ -1,4 +1,27 @@
-{pkgs, ...}: {
-  home.packages = with pkgs; [
-  ];
+{
+  pkgs,
+  pkgs-stable,
+  ...
+}: {
+  home.packages =
+    (with pkgs; [
+      obsidian
+      haguichi
+      kdePackages.kdenlive
+      delfin
+      xclicker
+      antimatter-dimensions
+      ghex
+      blockbench
+      legcord
+      blender-hip
+      parabolic
+      warp
+    ])
+    ++ (with pkgs-stable; [
+      aseprite
+      bottles
+      gnome-text-editor
+      unityhub
+    ]);
 }
