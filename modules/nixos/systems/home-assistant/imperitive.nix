e152c127e279bd622886a31917bddaf6eb66aa02
Techleaf <mikiediggins@gmail.com>
2026-01-21 17:25:08 +0000

    "860 2026-01-21 02:34:38 26.05.20260116.e4bae1b 6.18.5"

--- modules/nixos/systems/home-assistant/imperitive.nix
+++ modules/nixos/systems/home-assistant/imperitive.nix
@@ -5,7 +5,21 @@
 }: let
   cfg = config.home-assistant;
 in {
-  config =
-    lib.mkIf (cfg.enable && cfg.imperitive.enable) {
+  config = lib.mkIf (cfg.enable && cfg.imperitive.enable) {
+    virtualisation.oci-containers = lib.mkIf cfg.imperitive.oci {
+      backend = "podman";
+      containers.homeassistant = {
+        volumes = ["home-assistant:/config"];
+        environment.TZ = "Europe/London";
+        # Note: The image will not be updated on rebuilds, unless the version label changes
+        image = "ghcr.io/home-assistant/home-assistant:stable";
+        extraOptions = [
+          # Use the host network namespace for all sockets
+          "--network=host"
+          # Pass devices into the container, so Home Assistant can discover and make use of them
+          # "--device=/dev/ttyACM0:/dev/ttyACM0"
+        ];
+      };
     };
+  };
 }
