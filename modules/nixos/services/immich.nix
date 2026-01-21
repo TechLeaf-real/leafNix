08a0f30de3801369bc7b3dab3c54b1b6b87f5bca
Techleaf <mikiediggins@gmail.com>
2026-01-21 01:46:02 +0000

    "859 2026-01-20 01:58:17 26.05.20260116.e4bae1b 6.18.5"

--- modules/nixos/services/immich.nix
+++ modules/nixos/services/immich.nix
@@ -1,7 +1,7 @@
 {
   lib,
   config,
-  pkgs-stable,
+  # pkgs-stable,
   ...
 }: let
   cfg = config.immich;
@@ -15,7 +15,7 @@ in {
   config = lib.mkIf cfg.enable {
     services.immich = {
       enable = true;
-      package = pkgs-stable.immich;
+      # package = pkgs-stable.immich;
       mediaLocation = "/pool/dataset/immich";
       openFirewall = true;
       environment = {IMMICH_LOG_LEVEL = "verbose";};
