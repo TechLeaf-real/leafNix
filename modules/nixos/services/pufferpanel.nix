433c54aceacc43c9e6d9cb724f3dea3d39749fa8
Techleaf <mikiediggins@gmail.com>
2026-01-23 03:22:46 +0000

    "865 2026-01-22 04:22:57 26.05.20260120.80e4adb 6.18.6"

--- modules/nixos/services/pufferpanel.nix
+++ modules/nixos/services/pufferpanel.nix
@@ -15,16 +15,25 @@ in {
     services.pufferpanel = {
       enable = true;
       extraPackages = with pkgs; [bash curl gawk gnutar gzip];
-      # package = pkgs.buildFHSEnv {
-      #   name = "pufferpanel-fhs";
-      #   runScript = lib.getExe pkgs.pufferpanel;
-      #   targetPkgs = pkgs': with pkgs'; [icu openssl zlib];
-      # };
+      package = pkgs.buildFHSEnv {
+        name = "pufferpanel-fhs";
+        runScript = lib.getExe pkgs.pufferpanel;
+        targetPkgs = pkgs': with pkgs'; [icu openssl zlib];
+      };
       environment = {
         PUFFER_PANEL_REGISTRATIONENABLED = "false";
         PUFFER_PANEL_SETTINGS_COMPANYNAME = "LeafNet";
       };
     };
+
+    environment.systemPackages = with pkgs; [
+      buildFHSEnv
+      {
+        name = "pufferpanel-fhs";
+        runScript = lib.getExe pkgs.pufferpanel;
+        targetPkgs = pkgs': with pkgs'; [icu openssl zlib];
+      }
+    ];
 
     networking.firewall.allowedTCPPorts = [8080];
   };
