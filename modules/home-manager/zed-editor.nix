cb80d83e1bbf24ec0768d0da559a691f175c8efb
Techleaf <mikiediggins@gmail.com>
2026-01-27 08:00:47 +0000

    "884 2026-01-27 08:00:43 26.05.20260123.c5296fd 6.18.7"

--- modules/home-manager/zed-editor.nix
+++ modules/home-manager/zed-editor.nix
@@ -1,16 +1,16 @@
 {pkgs, ...}: {
   programs.zed-editor = {
-    enable = true;
+    enable = false;
     extraPackages = with pkgs; [
       nil
       nixd
     ];
 
     installRemoteServer = true;
-    # mutableUserDebug = false;
-    # mutableUserKeymaps = false;
-    # mutableUserSettings = false;
-    # mutableUserTasks = false;
+    mutableUserDebug = false;
+    mutableUserKeymaps = false;
+    mutableUserSettings = false;
+    mutableUserTasks = false;
     extensions = [
       "nix"
       "catppuccin-blur-plus"
@@ -19,9 +19,34 @@
       "catppuccin-icons"
     ];
 
-    # userDebug = [];
-    # userKeymaps = [];
-    # userSettings = [];
-    # userTasks = [];
+    userDebug = [];
+    userKeymaps = [];
+    userTasks = [];
+    userSettings = [
+      {
+        use_smartcase_search = true;
+        sticky_scroll.enabled = true;
+        autoscroll_on_clicks = true;
+        autosave.after_delay.milliseconds = 0;
+        auto_update = false;
+        telemetry = {
+          diagnostics = false;
+          metrics = false;
+        };
+        session.trust_all_worktrees = true;
+        disable_ai = true;
+        cursor_blink = true;
+        icon_theme = {
+          mode = "system";
+          light = "Catppuccin Latte";
+          dark = "Catppuccin Mocha";
+        };
+        theme = {
+          mode = "system";
+          light = "Catppuccin Latte (Blur) [Heavy]";
+          dark = "Catppuccin Mocha (Blur) [Heavy]";
+        };
+      }
+    ];
   };
 }
