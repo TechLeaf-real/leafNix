65f7c0720e22069402592e6fae7a12e6257a056a
Techleaf <mikiediggins@gmail.com>
2026-01-27 07:33:10 +0000

    "883 2026-01-27 07:33:06 26.05.20260123.c5296fd 6.18.7"

--- modules/home-manager/zed-editor.nix
+++ modules/home-manager/zed-editor.nix
@@ -7,10 +7,10 @@
     ];
 
     installRemoteServer = true;
-    mutableUserDebug = false;
-    mutableUserKeymaps = false;
-    mutableUserSettings = false;
-    mutableUserTasks = false;
+    # mutableUserDebug = false;
+    # mutableUserKeymaps = false;
+    # mutableUserSettings = false;
+    # mutableUserTasks = false;
     extensions = [
       "nix"
       "catppuccin-blur-plus"
@@ -19,9 +19,9 @@
       "catppuccin-icons"
     ];
 
-    userDebug = [];
-    userKeymaps = [];
+    # userDebug = [];
+    # userKeymaps = [];
     # userSettings = [];
-    userTasks = [];
+    # userTasks = [];
   };
 }
