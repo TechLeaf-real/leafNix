8a2bff3166047030fbc4a5db98d4f4f583977136
Techleaf <mikiediggins@gmail.com>
2026-01-30 13:33:37 +0000

    "902 2026-01-30 13:33:34 26.05.20260126.bfc1b8a 6.18.7"

--- modules/nixos/programs/vscodium.nix
+++ modules/nixos/programs/vscodium.nix
@@ -1,32 +1,29 @@
 {
   lib,
   config,
-  pkgs,
   ...
-}: let
-  cfg = config.vscodium;
-in {
+}: {
   options = {
     vscodium = {
       enable = lib.mkEnableOption "vscodium";
     };
   };
 
-  config = lib.mkIf cfg.enable {
-    environment.systemPackages = with pkgs; [
-      alejandra
-      (vscode-with-extensions.override {
-        vscode = vscodium;
-        vscodeExtensions = with vscode-extensions; [
-          jnoortheen.nix-ide
-          rust-lang.rust-analyzer
-          catppuccin.catppuccin-vsc-icons
-          catppuccin.catppuccin-vsc
-          kamadorueda.alejandra
-          mkhl.direnv
-          vadimcn.vscode-lldb
-        ];
-      })
-    ];
-  };
+  # config = lib.mkIf cfg.enable {
+  #   environment.systemPackages = with pkgs; [
+  #     alejandra
+  #     (vscode-with-extensions.override {
+  #       vscode = vscodium;
+  #       vscodeExtensions = with vscode-extensions; [
+  #         jnoortheen.nix-ide
+  #         rust-lang.rust-analyzer
+  #         catppuccin.catppuccin-vsc-icons
+  #         catppuccin.catppuccin-vsc
+  #         kamadorueda.alejandra
+  #         mkhl.direnv
+  #         vadimcn.vscode-lldb
+  #       ];
+  #     })
+  #   ];
+  # };
 }
